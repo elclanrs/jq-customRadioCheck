c615933a750e6c88f7401710274ae41913c5dec4
elclanrs <elclanrs@gmail.com>
2012-10-02 03:25:32 -0400

    initial commit

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ A tiny jQuery plugin to customize `checkbox` and `radio` inputs.
 **Support:** IE8+, Webkit, Firfox, Opera, iOS5+, Android 4.0+.  
 **jQuery:** 1.7+  
 **License:** MIT
+
+## Usage:
+
+```javascript
+$('input[type=checkbox], input[type=radio]').customRadioCheck();
+```
---
+++ customRadioCheck.js
@@ -0,0 +1,37 @@
+/*
+ * customRadioCheck: jQuery plguin for checkbox and radio replacement
+ * Usage: $('input[type=checkbox], input[type=radio]').customRadioCheck();
+ * Author: Cedric Ruiz
+ * License: MIT
+ */
+;(function(){
+$.fn.customRadioCheck = function() {
+  
+  return this.each(function() {
+
+    var $this = $(this);
+    var $span = $('<span/>');
+
+    $span.addClass('custom-'+ ($this.is(':checkbox') ? 'check' : 'radio'));
+    $this.is(':checked') && $span.addClass('checked'); // init
+    $span.insertAfter($this);
+
+    $this.parent('label').addClass('custom-label').attr('onclick', ''); // Fix clicking label in iOS
+    $this.css({ position: 'absolute', left: '-9999px' }); // hide by shifting left
+    
+    // Events
+    $this.on({
+      change: function() {
+        var $this = $(this);
+        if ($this.is(':radio')) {
+          $this.parent().siblings('label').find('.custom-radio').removeClass('checked');
+        }
+        $span.toggleClass('checked');
+      },
+      focus: function() { $span.addClass('focus'); },
+      blur: function() { $span.removeClass('focus'); },
+      click: function() { $(this).trigger('focus'); }
+    });
+  });
+};
+}());
